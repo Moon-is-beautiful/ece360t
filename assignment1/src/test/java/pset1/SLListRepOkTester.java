3c9c79cb6a6fa2c50db4c9d4a079c6320d44fc01
doge <hzh853240936@gmail.com>
2024-09-19 21:27:27 -0500

    second commit

--- assignment1/src/test/java/pset1/SLListRepOkTester.java
+++ assignment1/src/test/java/pset1/SLListRepOkTester.java
@@ -16,6 +16,8 @@ public class SLListRepOkTester {
         SLList l = new SLList();
         Node n = new Node();
         // your code goes here
+        n.elem=true;
+        n.next=null;
         assertTrue(l.repOk());
         l.last=n;
         assertFalse(l.repOk());
@@ -27,22 +29,27 @@ public class SLListRepOkTester {
         Node n = new Node();
         // your code goes here
         assertTrue(l.repOk());
+        n.elem=true;
+        n.next=n;
         l.first=n;
-        l.first.next=n;
         l.last=l.first;
         assertFalse(l.repOk());
     }
 
     @Test
-    public void t2() {
+    public void t3() {
         SLList l = new SLList();
         Node n = new Node();
         // your code goes here
         assertTrue(l.repOk());
-        l.first=n;
-        l.first.next=n;
-        l.last=l.first;
-        assertFalse(l.repOk());
+        Node m= new Node();
+        m.elem=true;
+        m.next=n;
+        n.elem=true;
+        n.next=null;
+        l.first=m;
+        l.last=n;
+        assertTrue(l.repOk());
     }
     
     // your code goes here
